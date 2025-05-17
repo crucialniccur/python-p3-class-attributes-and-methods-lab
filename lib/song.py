104e9179d759028414e086c00dae8df1ab6b8cb7
Mati Joseph <crucialniccur@gmail.com>
2025-05-17 06:19:01 +0300

    define the add_song_to_count method

--- lib/song.py
+++ lib/song.py
@@ -2,7 +2,12 @@ class Song:
 
     count = 0
 
-    def __init__(self, name, artist, genre):
+    def __init__(self, name, artist, genres):
+        self.add_song_to_count()
         self.name = name
         self.artist = artist
-        self.genre = genre
+        self.genre = genres
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
